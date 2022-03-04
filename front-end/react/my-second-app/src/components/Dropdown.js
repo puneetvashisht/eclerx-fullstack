b526b64a95cb5b7d39b2f1e3c9035f21a5c403a1
i <puneetvashisht@gmail.com>
2022-03-04 15:39:42 +0530

    list display

--- front-end/react/my-second-app/src/components/Dropdown.js
+++ front-end/react/my-second-app/src/components/Dropdown.js
@@ -8,19 +8,28 @@ export default function Dropdown(props) {
   const [courses, setCourses] = useState([
       'React', 'Angular', 'NodeJS', 'Test'
   ])
+
+  const [label, setLabel] = useState(props.label)
   // changing the state... show->true->false
   const toggleShow = () => {
         setShow(!show);
         console.log(show);
+       
   }
 
-  let coursesList = courses.map((course, i) => <li><a className="dropdown-item" href="#">{course}</a></li>);
+  const handleItemClick = (course)=>{
+      console.log('Item clicked...', course);
+      setLabel(course);
+      setShow(false);
+  }
+
+  let coursesList = courses.map((course, i) => <li><a onClick={()=>handleItemClick(course)} className="dropdown-item" href="#">{course}</a></li>);
   console.log(coursesList);
 
   return (
     <div className="dropdown">
   <button onClick={toggleShow} className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-    {props.label}
+    {label}
   </button>
   <ul className={show?'dropdown-menu show': 'dropdown-menu'} aria-labelledby="dropdownMenuButton1">
      {coursesList}
--- front-end/react/my-second-app/src/index.js
+++ front-end/react/my-second-app/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
+import Clock from './components/Clock';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +12,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// setInterval(()=>{
+//   ReactDOM.render(<Clock today={new Date()}></Clock>,document.getElementById('root'))
+// }, 1000)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
